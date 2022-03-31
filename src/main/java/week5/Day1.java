9aed702043929f0a25e6449681c12dc52f8732e1
ckhemani <chandru.khemani@gmail.com>
2022-03-31 07:15:36 -0400

    updated Day1 Class for some test

--- src/main/java/week5/Day1.java
+++ src/main/java/week5/Day1.java
@@ -2,4 +2,17 @@ package week5;
 
 public class Day1 {
     //Progress Checks
+
+//    echo "# fooddeliveryrepo" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/ckhemani/fooddeliveryrepo.git
+//    git push -u origin main
+
+//…or push an existing repository from the command line
+//    git remote add origin https://github.com/ckhemani/fooddeliveryrepo.git
+//    git branch -M main
+//    git push -u origin main
 }
